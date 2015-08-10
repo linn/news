498a569e4c0b14afbaaef3c2433ca6473ff086fb
Barry Williams <barry.williams@linn.co.uk>
2015-08-10 15:49:53 +0100

    Added release configuration

---
+++ config/release.js
@@ -0,0 +1,17 @@
+"use strict";
+
+module.exports = {
+    showStackTraceOnError: true,
+    awsRegion: 'eu-west-1',
+    dynamoDb: {
+        tables: {
+            news: "linn.cloud.news"
+        },
+        indices: {}
+    },
+    s3: {
+        buckets: {
+            newsAttachments: "linn.cloud.news.attachments"
+        }
+    }
+};
