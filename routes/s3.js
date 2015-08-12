0a8df5d10992c1f61ecb54b40d431ab5771d71ef
Barry Williams <barry.williams@linn.co.uk>
2015-08-12 15:37:43 +0100

    Image upload bug fixed when in edit mode Image upload now uses GUIDs

--- routes/s3.js
+++ routes/s3.js
@@ -2,30 +2,29 @@
 var multiparty = require('multiparty');
 var fs = require('fs');
 var config = require('linn-cloud-libs/config');
+var uuid = require('node-uuid');
 
 var aws = require('aws-sdk');
 aws.config.region = config.awsRegion;
 
 var s3 = new aws.S3();
 
-var AWS = require('aws-sdk');
-
 function generateUri(key) {
     return 'http://' + config.s3.buckets.newsAttachments + ".s3.amazonaws.com/" + key;
 }
 
-exports.uploadImage = function streamToS3(req, res, next) {
+function streamToS3(req, res, next) {
     var form = new multiparty.Form();
     form.on('part', function(part) {
         var params = {
             Bucket: config.s3.buckets.newsAttachments,
-            Key: part.filename,
+            Key: uuid.v1(),
             Body: part,
             ContentDisposition: 'attachment; filename=' + part.filename,
             ContentType: part.headers['content-type'],
             ContentLength: part.byteCount
         };
-        s3.putObject(params, function(err, data) {
+        s3.putObject(params, function(err) {
             if (err) {
                 next(err);
             } else {
@@ -35,3 +34,4 @@ exports.uploadImage = function streamToS3(req, res, next) {
     });
     form.parse(req);
 }
+exports.uploadImage = streamToS3;
